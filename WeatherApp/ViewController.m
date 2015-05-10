5cb2854ecc4d068ea39803a5056e0b0ed39ece15
Stephanie Schiniou <supersteph@unknown-b8-e8-56-42-ff-e8.home>
2015-05-10 21:56:04 +0100

    Finds location & shows on screen. Storyboard needs work

--- WeatherApp/ViewController.m
+++ WeatherApp/ViewController.m
@@ -10,22 +10,76 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *mCoordinateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *mLatitude;
+@property (weak, nonatomic) IBOutlet UILabel *mLongitude;
+@property (weak, nonatomic) IBOutlet UILabel *mAddress;
 
 @end
 
 @implementation ViewController
 @synthesize mCoordinateLabel;
+@synthesize mLatitude;
+@synthesize mLongitude;
+@synthesize mAddress;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    mLocationManager = [[CLLocationManager alloc] init];
     
     mCoordinateLabel.text = @"The weather is: ";
+}
+
+#define IS_OS_8_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+
+- (IBAction)mGetCurrentLocation:(id)sender {
+    mLocationManager.delegate = self;
+    mLocationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    if(IS_OS_8_OR_LATER){
+        NSUInteger code = [CLLocationManager authorizationStatus];
+        if (code == kCLAuthorizationStatusNotDetermined && ([mLocationManager respondsToSelector:@selector(requestAlwaysAuthorization)] || [mLocationManager respondsToSelector:@selector(requestWhenInUseAuthorization)])) {
+            // choose one request according to your business.
+            if([[NSBundle mainBundle] objectForInfoDictionaryKey:@"NSLocationAlwaysUsageDescription"]){
+                [mLocationManager requestAlwaysAuthorization];
+            } else if([[NSBundle mainBundle] objectForInfoDictionaryKey:@"NSLocationWhenInUseUsageDescription"]) {
+                [mLocationManager requestWhenInUseAuthorization];
+            } else {
+                NSLog(@"Info.plist does not contain NSLocationAlwaysUsageDescription or NSLocationWhenInUseUsageDescription");
+            }
+        }
+    }
+    
+    [mLocationManager startUpdatingLocation];
+}
+
+
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"didFailWithError: %@", error);
+    UIAlertView *errorAlert = [[UIAlertView alloc]
+                               initWithTitle:@"Error" message:@"Failed to Get Your Location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [errorAlert show];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    NSLog(@"didUpdateToLocation: %@", newLocation);
+    CLLocation *currentLocation = newLocation;
+    
+    if (currentLocation != nil) {
+        mLongitude.text = [NSString stringWithFormat:@"Long: %.8f", currentLocation.coordinate.longitude];
+        mLatitude.text = [NSString stringWithFormat:@"Lat: %.8f", currentLocation.coordinate.latitude];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
