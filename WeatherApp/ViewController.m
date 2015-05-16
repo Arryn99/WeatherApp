3a3e255f6ea12155da9bfdc2654f0334ef410e33
Stephanie Schiniou <supersteph@unknown-b8-e8-56-42-ff-e8.home>
2015-05-16 17:10:15 +0100

    Get the weather for London today

--- WeatherApp/ViewController.m
+++ WeatherApp/ViewController.m
@@ -26,7 +26,33 @@
     mLocationManager = [[CLLocationManager alloc] init];
     
     mCoordinateLabel.text = @"The weather is: ";
+    
+    //this call is synchronous and takes too long
+    NSData *allCoursesData = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString:@"http://api.openweathermap.org/data/2.5/weather?q=London,uk"]];
+    NSString* returnedValue = [[NSString alloc] initWithData: allCoursesData encoding:NSUTF8StringEncoding];
+    NSLog(@"%@", returnedValue);
+    
+    NSError *error = nil;
+    
+    //parse json
+    id object = [NSJSONSerialization JSONObjectWithData:allCoursesData options:NSJSONReadingAllowFragments error:&error];
+    
+    if(error) {
+        NSLog(@"There was an error parsing JSON");
+    }
+    else {
+        if([object isKindOfClass:[NSDictionary class]]) {
+            NSDictionary *parsedJSON = (NSDictionary*) object;
+            for(NSString *key in [parsedJSON allKeys]) {
+                NSLog(@"%@",[parsedJSON objectForKey:key]);
+            }
+            NSDictionary *main = [parsedJSON objectForKey:@"main"];
+            NSNumber *temp = [main objectForKey: @"temp"];
+        }
+           
+    }
 }
+
 
 #define IS_OS_8_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
 
